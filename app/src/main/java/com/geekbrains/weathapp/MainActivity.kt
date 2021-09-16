beb66fc7783b80fa6c853c2f5a685686917c9062
Kovalev Ivan <j.j.cherdak@gmail.com>
2021-09-16 23:41:53 +0300

    Initial commit

---
+++ app/src/main/java/com/geekbrains/weathapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.geekbrains.weathapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geekbrains.weathapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/geekbrains/weathapp/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.geekbrains.weathapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
